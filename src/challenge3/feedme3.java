f84c254f231637cb2fde4d76555fceae968ec90f
Devore360 <106493979+Devore360@users.noreply.github.com>
2022-07-13 15:04:55 -0500

    071322

---
+++ src/challenge3/feedme3.java
@@ -0,0 +1,35 @@
+package challenge3;
+
+// Java programs for ArrayList, getting Random Elements, and scanner
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class feedme3 {
+    public static void main(String[] args) {
+        //Creating ArrayList
+        ArrayList<String> Dinner = new ArrayList<String>();
+
+        //Making new Scanner class object, stored in the variable input
+        Scanner sc = new Scanner(System.in);
+
+        //Prompt user for input while i is less than 3.
+        int i = 0;
+        while (sc.Next())
+        {
+            System.out.println("Respond end when done. Please enter dinner option #" + (i + 1));
+            String answer = sc.nextLine();
+            Dinner.add(answer);
+            i++;
+        }
+
+        //Randomizing the arraylist to print output
+        Random r = new Random();
+        {
+            int randomitem = r.nextInt(Dinner.size());
+            String randomElement = Dinner.get(randomitem);
+            System.out.println("Random Element = " + randomElement);
+        }
+
+    }
+}
